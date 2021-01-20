d43e14aa7b2dce18683b51788a464963f9a113cf
Gokhan Donmez <donmez.gokhan@yahoo.com>
2021-01-20 23:54:00 +0300

    Add project files.

---
+++ Collections/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] names = new string[] { "Engin", "Gökhan", "Kaan" };
+            //Console.WriteLine(names[0]);
+            //Console.WriteLine(names[1]);
+            //Console.WriteLine(names[2]);
+
+            List<string> names2 = new List<string> { "Engin", "Gökhan", "Kaan" };
+            Console.WriteLine(names2[0]);
+            Console.WriteLine(names2[1]);
+            Console.WriteLine(names2[2]);
+            names2.Add("Araba");
+            Console.WriteLine(names2[3]);
+            Console.WriteLine(names2[0]);
+
+        }
+    }
+}
